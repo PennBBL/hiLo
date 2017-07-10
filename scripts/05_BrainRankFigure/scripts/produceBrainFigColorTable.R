9421a7fac4ab939b96490cde142b3d45a6010e56
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-07-10 14:25:15 -0400

    Updated script to return hi-me-lo diff as opposed to beta values

--- scripts/05_BrainRankFigure/scripts/produceBrainFigColorTable.R
+++ scripts/05_BrainRankFigure/scripts/produceBrainFigColorTable.R
@@ -5,23 +5,17 @@
 source('/home/adrose/hiLo/scripts/05_BrainRankFigure/functions/functions.R')
 
 ## Load data
-maleData <- read.csv('/home/adrose/hiLo/data/04_CognitiveModels/maleFitBetas.csv')
-femaleData <- read.csv('/home/adrose/hiLo/data/04_CognitiveModels/femaleFitBetas.csv')
+maleDataVol <- read.csv('/home/angelgar/highMedLow/output_hi-me-low_vol_male.csv')
+maleDataGmd <- read.csv('/home/angelgar/highMedLow/output_hi-me-low_gmd_male.csv')
+maleDataCbf <- read.csv('/home/angelgar/highMedLow/output_hi-me-low_cbf_male.csv')
+femaleDataVol <- read.csv('/home/angelgar/highMedLow/output_hi-me-low_vol_female.csv')
+femaleDataGmd <- read.csv('/home/angelgar/highMedLow/output_hi-me-low_gmd_female.csv')
+femaleDataCbf <- read.csv('/home/angelgar/highMedLow/output_hi-me-low_cbf_female.csv')
 
 ## Now produce all of the modality & gender specific color tables 
-writeColorTableandKey(inputData=maleData, inputColumn=2, outName='volMale')
-writeColorTableandKey(inputData=maleData, inputColumn=3, outName='cbfMale')
-writeColorTableandKey(inputData=maleData, inputColumn=4, outName='gmdMale')
-writeColorTableandKey(inputData=maleData, inputColumn=5, outName='ctMale')
-writeColorTableandKey(inputData=maleData, inputColumn=6, outName='rehoMale')
-writeColorTableandKey(inputData=maleData, inputColumn=7, outName='alffMale')
-writeColorTableandKey(inputData=maleData, inputColumn=8, outName='trMale')
-
-writeColorTableandKey(inputData=femaleData, inputColumn=2, outName='volfeMale')
-writeColorTableandKey(inputData=femaleData, inputColumn=3, outName='cbffeMale')
-writeColorTableandKey(inputData=femaleData, inputColumn=4, outName='gmdfeMale')
-writeColorTableandKey(inputData=femaleData, inputColumn=5, outName='ctfeMale')
-writeColorTableandKey(inputData=femaleData, inputColumn=6, outName='rehofeMale')
-writeColorTableandKey(inputData=femaleData, inputColumn=7, outName='alfffeMale')
-writeColorTableandKey(inputData=femaleData, inputColumn=8, outName='trfeMale')
-
+writeColorTableandKey(inputData=maleDataVol, inputColumn=5, outName='volMale')
+writeColorTableandKey(inputData=maleDataGmd, inputColumn=5, outName='gmdMale')
+writeColorTableandKey(inputData=maleDataCbf, inputColumn=5, outName='cbfMale')
+writeColorTableandKey(inputData=femaleDataVol, inputColumn=5, outName='volFemale')
+writeColorTableandKey(inputData=femaleDataGmd, inputColumn=5, outName='gmdFemale')
+writeColorTableandKey(inputData=femaleDataCbf, inputColumn=5, outName='cbfFemale')
