e29c8efa91ad91bb16bf74bb6d1a32bc84da2fc5
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-10-03 17:08:34 -0400

    Fixed CC range

--- scripts/03_CognitiveTrends/scripts/mgiRep.R
+++ scripts/03_CognitiveTrends/scripts/mgiRep.R
@@ -13,19 +13,19 @@ install_load('plyr', 'ggplot2', 'reshape2', 'grid', 'gridExtra', 'labeling', 'da
 vol.modal.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRMGI/volumeData.csv')
 vol.modal.data$F1_Exec_Comp_Cog_Accuracy <- vol.modal.data$Overall_EfficiencyAR
 gmd.modal.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRMGI/gmdData.csv')
-gmd.modal.data$F1_Exec_Comp_Cog_Accuracy <- gmd.modal.data$Overall_Efficiency
+gmd.modal.data$F1_Exec_Comp_Cog_Accuracy <- gmd.modal.data$Overall_EfficiencyAR
 ct.modal.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRMGI/ctData.csv')
-ct.modal.data$F1_Exec_Comp_Cog_Accuracy <- ct.modal.data$Overall_Efficiency
+ct.modal.data$F1_Exec_Comp_Cog_Accuracy <- ct.modal.data$Overall_EfficiencyAR
 cc.modal.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRMGI/ccData.csv')
-cc.modal.data$F1_Exec_Comp_Cog_Accuracy <- cc.modal.data$Overall_Efficiency
+cc.modal.data$F1_Exec_Comp_Cog_Accuracy <- cc.modal.data$Overall_EfficiencyAR
 vol.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeRegMGI/volumeData.csv')
-vol.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- vol.modal.data.age.reg$Overall_Efficiency
+vol.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- vol.modal.data.age.reg$Overall_EfficiencyAR
 gmd.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeRegMGI/gmdData.csv')
-gmd.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- gmd.modal.data.age.reg$Overall_Efficiency
+gmd.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- gmd.modal.data.age.reg$Overall_EfficiencyAR
 ct.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeRegMGI/ctData.csv')
-ct.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- ct.modal.data.age.reg$Overall_Efficiency
+ct.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- ct.modal.data.age.reg$Overall_EfficiencyAR
 cc.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeRegMGI/ccData.csv')
-cc.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- cc.modal.data.age.reg$Overall_Efficiency
+cc.modal.data.age.reg$F1_Exec_Comp_Cog_Accuracy <- cc.modal.data.age.reg$Overall_EfficiencyAR
 
 vol.modal.data.age.reg$ageBin <- 'Age Regressed'
 gmd.modal.data.age.reg$ageBin <- 'Age Regressed'
@@ -51,7 +51,7 @@ age.reg.cc <- doEverythingEverCT(cc.modal.data.age.reg, 'mprage_jlf_cortcon', 0,
 volPlotAgeReg <- createGGPlotImage(age.reg.vol, 'Volume Hi-Lo JLF Data Age Reg', -1, 1, .2)
 gmdPlotAgeReg <- createGGPlotImage(age.reg.gmd, 'GMD Hi-Lo JLF Data Age Reg', -1, 1, .2)
 ctPlotAgeReg <- createGGPlotImage(age.reg.ct, 'CT Hi-Lo JLF Data Age Reg', -1, 1, .2)
-ccPlotAgeReg <- createGGPlotImage(age.reg.cc, 'CC Hi-Lo JLF Data Age Reg', -1.2, 1, .2)
+ccPlotAgeReg <- createGGPlotImage(age.reg.cc, 'CC Hi-Lo JLF Data Age Reg', -1, 1, .2)
 
 pdf('ageRegHi-LoGraphsMGI.pdf', width=20, height=20)
 volPlotAgeReg
