5d0019e5fc7f1adc2ed8db1bcd4ff460c43d936d
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-11-06 12:58:54 -0500

    Seeing very odd CT effects here

--- scripts/02_DevelopmentalTrends/scripts/apriorAgeBin.R
+++ scripts/02_DevelopmentalTrends/scripts/apriorAgeBin.R
@@ -32,6 +32,39 @@ volume.data.1 <- rbind(returnStandardizedGenderMeans(vol.data, "mprage_jlf_vol",
 volume.data.1$ageBin <- factor(volume.data.1$ageBin, levels=c('56+', '29-55','Early Adulthood','Adolescence','Childhood'))
 volumePlot <- createGGPlotImage(volume.data.1, 'Volume', -2, 2, .2)
 
+gmd.data <- gmd.data[,(names(gmd.data) %in% names(gmd.data.m))]
+gmd.data.m <- gmd.data.m[,(names(gmd.data.m) %in% names(gmd.data))]
+
+# Now combine the data
+gmd.data <- rbind(gmd.data, gmd.data.m)
+gmd.data$ageBin <- 'Childhood'
+gmd.data$ageBin[gmd.data$ageAtGo1Scan > 14.1] <- 'Adolescence'
+gmd.data$ageBin[gmd.data$ageAtGo1Scan > 18.1] <- 'Early Adulthood'
+gmd.data$ageBin[gmd.data$ageAtGo1Scan > 29.1] <- '29-55'
+gmd.data$ageBin[gmd.data$ageAtGo1Scan > 56] <- '56+'
+
+gmd.data.1 <- rbind(returnStandardizedGenderMeans(gmd.data, "mprage_jlf_gmd", "","Childhood"), returnStandardizedGenderMeans(gmd.data, "mprage_jlf_gmd", "","Adolescence"), returnStandardizedGenderMeans(gmd.data, "mprage_jlf_gmd", "","Early Adulthood"), returnStandardizedGenderMeans(gmd.data, "mprage_jlf_gmd", "","29-55"), returnStandardizedGenderMeans(gmd.data, "mprage_jlf_gmd", "","56+"))
+gmd.data.1$ageBin <- factor(gmd.data.1$ageBin, levels=c('56+', '29-55','Early Adulthood','Adolescence','Childhood'))
+gmdPlot <- createGGPlotImage(gmd.data.1, 'gmd', -2, 2.2, .2)
+
+
+ct.data <- ct.data[,(names(ct.data) %in% names(ct.data.m))]
+ct.data.m <- ct.data.m[,(names(ct.data.m) %in% names(ct.data))]
+
+# Now combine the data
+ct.data <- rbind(ct.data, ct.data.m)
+ct.data$ageBin <- 'Childhood'
+ct.data$ageBin[ct.data$ageAtGo1Scan > 14.1] <- 'Adolescence'
+ct.data$ageBin[ct.data$ageAtGo1Scan > 18.1] <- 'Early Adulthood'
+ct.data$ageBin[ct.data$ageAtGo1Scan > 29.1] <- '29-55'
+ct.data$ageBin[ct.data$ageAtGo1Scan > 56] <- '56+'
+
+ct.data.1 <- rbind(returnStandardizedGenderMeansCT(ct.data, "mprage_jlf_ct", "","Childhood"), returnStandardizedGenderMeansCT(ct.data, "mprage_jlf_ct", "","Adolescence"), returnStandardizedGenderMeansCT(ct.data, "mprage_jlf_ct", "","Early Adulthood"), returnStandardizedGenderMeansCT(ct.data, "mprage_jlf_ct", "","29-55"), returnStandardizedGenderMeansCT(ct.data, "mprage_jlf_ct", "","56+"))
+ct.data.1$ageBin <- factor(ct.data.1$ageBin, levels=c('56+', '29-55','Early Adulthood','Adolescence','Childhood'))
+ctPlot <- createGGPlotImage(ct.data.1, 'ct', -2.2, 2, .2)
+
 pdf('developmentalFigures.pdf' ,height=34, width=20)
 volumePlot
+gmdPlot
+ctPlot
 dev.off()
