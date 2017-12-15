90385bd7976393647f613162e9f6d2bd30b66caf
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-12-15 15:59:42 -0500

    Added WM Labels, and reho figure exporting

--- scripts/03_CognitiveTrends/scripts/hiLoFinalExplore.R
+++ scripts/03_CognitiveTrends/scripts/hiLoFinalExplore.R
@@ -1,5 +1,3 @@
-# This script is going to be used to produce the hi - lo graphs from jlf data
-# hi - lo will just not die....
 ## Load Library(s)
 source("/home/adrose/adroseHelperScripts/R/afgrHelpFunc.R")
 source("/home/adrose/hiLo/scripts/03_CognitiveTrends/functions/wm2Functions.R")
@@ -7,40 +5,40 @@ source("/home/adrose/hiLo/scripts/03_CognitiveTrends/functions/wm1Functions2.R")
 source("/home/adrose/hiLo/scripts/03_CognitiveTrends/functions/functions-forJLF.R")
 install_load('plyr', 'ggplot2', 'reshape2', 'grid', 'gridExtra', 'labeling', 'data.table')
 
-# Declare any functions
-# Now load the data
+## Load data
 vol.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeReg/volumeData.csv')
 cbf.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeReg/cbfData.csv')
 gmd.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeReg/gmdData.csv')
 tr.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeReg/jlfTRData.csv')
 
+## Prep data
+cbf.modal.data.age.reg$ageBin <- 'Age Regressed'
 vol.modal.data.age.reg$ageBin <- 'Age Regressed'
 gmd.modal.data.age.reg$ageBin <- 'Age Regressed'
-cbf.modal.data.age.reg$ageBin <- 'Age Regressed'
 tr.modal.data.age.reg$ageBin <- 'Age Regressed'
 
+# Now I need to grab the effect sizes
 ## Now prep the data 
-age.reg.vol <- doEverythingEverZ(vol.modal.data.age.reg, 'mprage_jlf_vol', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
+age.reg.vol <- doEverythingEver(vol.modal.data.age.reg, 'mprage_jlf_vol', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
 age.reg.vol$modal <- 'Volume'
-age.reg.cbf <- doEverythingEverZ(cbf.modal.data.age.reg, 'pcasl_jlf_cbf', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
+age.reg.cbf <- doEverythingEver(cbf.modal.data.age.reg, 'pcasl_jlf_cbf', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
 age.reg.cbf$modal <- 'CBF'
-age.reg.gmd <- doEverythingEverZ(gmd.modal.data.age.reg, 'mprage_jlf_gmd', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
+age.reg.gmd <- doEverythingEver(gmd.modal.data.age.reg, 'mprage_jlf_gmd', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
 age.reg.gmd$modal <- 'GMD'
-age.reg.tr <- doEverythingEverZ(tr.modal.data.age.reg, 'dti_jlf_tr', 0, 167, 'Age Regressed', cerebellum=F,optionalRace=NULL)
+age.reg.tr <- doEverythingEver(tr.modal.data.age.reg, 'dti_jlf_tr', 0, 167, 'Age Regressed', cerebellum=F,optionalRace=NULL)
 age.reg.tr$modal <- 'TR'
 
-#Now combine all data 
+# Now I need to prepare the values for one graph
 toPlot <- rbind(age.reg.vol, age.reg.cbf, age.reg.gmd, age.reg.tr)
 toPlot$modal <- factor(toPlot$modal, levels=c('Volume', 'GMD', 'CBF', 'TR'))
-toPlot$group <- paste(toPlot$groupLevel, toPlot$Gender)
-plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=group, color=group)) +
+plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=sex, color=sex)) +
       geom_line( size=5) +
       geom_point(size=1.5) +
       xlab("ROI") +
       ylab("z-score") +
       geom_hline(aes(yintercept=0), linetype="longdash", colour="black", size=0.5) +
-      scale_colour_manual(name = "group",
-                          values=c("hi Male"="blue1","hi Female"="red1", "lo Male"="turquoise", "lo Female"="orange")) +
+      scale_colour_manual(name = "sex",
+                          values=c("M"="blue1","F"="red1")) +
       theme_bw() +
       theme(legend.position="top") +
       theme(text=element_text(size=20), axis.text.x = element_text(angle = 45, hjust = 1, face="bold"), 
@@ -50,7 +48,8 @@ plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=group, color=group)
       coord_cartesian(ylim = c(-.6, .6)) + 
       facet_grid(modal ~ lobe, scales="free", space="free_x")
 
-pdf('testingHiLo.pdf', height=20, width=20)
+
+pdf('testingHiLoEffectSize.pdf', height=20, width=20)
 plotToReturn
 dev.off()
 
@@ -62,18 +61,17 @@ reho.modal.data.age.reg <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/mean
 reho.modal.data.age.reg$ageBin <- 'Age Regressed'
 
 # Grab the z scores
-age.reg.reho <- doEverythingEverZ(reho.modal.data.age.reg, 'rest_jlf_reho', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
+age.reg.reho <- doEverythingEver(reho.modal.data.age.reg, 'rest_jlf_reho', 0, 999, 'Age Regressed', cerebellum=F,optionalRace=NULL)
 age.reg.reho$modal <- 'ReHo'
 toPlot <- age.reg.reho
-toPlot$group <- paste(toPlot$groupLevel, toPlot$Gender)
-plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=group, color=group)) +
+plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=sex, color=sex)) +
       geom_line( size=5) +
       geom_point(size=1.5) +
       xlab("ROI") +
       ylab("z-score") +
       geom_hline(aes(yintercept=0), linetype="longdash", colour="black", size=0.5) +
       scale_colour_manual(name = "group",
-                          values=c("hi Male"="blue1","hi Female"="red1", "lo Male"="turquoise", "lo Female"="orange")) +
+                          values=c("M"="blue1","F"="red1")) +
       theme_bw() +
       theme(legend.position="top") +
       theme(text=element_text(size=20), axis.text.x = element_text(angle = 45, hjust = 1, face="bold"), 
@@ -83,26 +81,24 @@ plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=group, color=group)
       coord_cartesian(ylim = c(-.6, .6)) + 
       facet_grid(modal ~ lobe, scales="free", space="free_x")
 
-pdf('testingHiLoReho.pdf', height=6, width=20)
+pdf('testingHiLoEffectSizeReho.pdf', height=6, width=20)
 plotToReturn
 dev.off()
 
-
-## Now do the wm labels
+# Now create the wm tract values
+# Create the do everything function for WM tracts
 doEverythingEver <- function(df, modalityGrepPattern, lowerAge.e, upperAge.e, ageBinName.e){
   #tmp <- addAgeBins(df$ageAtGo1Scan, df, lowerAge.e, upperAge.e, ageBinName.e)
   tmp <- standardizePerfGroups(df, modalityGrepPattern, ageBinName.e)
   tmp <- organizeWM1ROINames(tmp)
-  #tmp <- subtractHiFromLo(tmp)
+  tmp <- subtractHiFromLo(tmp)
   tmp$gender <- revalue(tmp$gender, c('1'='Male', '2'='Female'))
-  tmp <- tmp[-which(tmp$groupLevel=='me'),]
   tmp$meanValue <- as.numeric(as.character(tmp$meanValue))
   colnames(tmp)[6] <- 'Gender'
   levels(tmp$ageBin) <- rev(levels(tmp$ageBin))
   return(tmp)
 }
 
-# Now load the data
 fa.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeReg/jhuFALabelsData.csv')
 tr.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeReg/jhuTRLabelsData.csv')
 fa.data$ageBin <- 'Age Regressed'
@@ -115,15 +111,15 @@ tr.data.age.reg$modal <- 'TR'
 # Now plot em
 # Now I need to prepare the values for one graph
 toPlot <- rbind(fa.data.age.reg, tr.data.age.reg)
-toPlot$group <- paste(toPlot$groupLevel, toPlot$Gender)
-plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=group, color=group)) +
+toPlot$modal <- factor(toPlot$modal, levels=c('FA', 'TR'))
+plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=sex, color=sex)) +
       geom_line( size=5) +
       geom_point(size=1.5) +
       xlab("ROI") +
       ylab("z-score") +
       geom_hline(aes(yintercept=0), linetype="longdash", colour="black", size=0.5) +
-      scale_colour_manual(name = "group",
-                          values=c("hi Male"="blue1","hi Female"="red1", "lo Male"="turquoise", "lo Female"="orange")) +
+      scale_colour_manual(name = "sex",
+                          values=c("M"="blue1","F"="red1")) +
       theme_bw() +
       theme(legend.position="top") +
       theme(text=element_text(size=20), axis.text.x = element_text(angle = 45, hjust = 1, face="bold"), 
@@ -134,7 +130,6 @@ plotToReturn <- ggplot(toPlot, aes(y=meanValue, x=ROI, group=group, color=group)
       facet_grid(modal ~ lobe, scales="free", space="free_x")
 
 
-pdf('testingHiLoLabels.pdf', height=10, width=12)
+pdf('testingHiLoEffectSizeWMLabels.pdf', height=10, width=12)
 plotToReturn
 dev.off()
-
