907e3e9c441905922e2c32e6496a62de1ca3a621
Adon Rosen <adrose@galton.uphs.upenn.edu>
2018-02-27 18:42:50 -0500

    Added lobular values to the mega csv

--- scripts/01_DataPrep/scripts/produceMegaCsv.R
+++ scripts/01_DataPrep/scripts/produceMegaCsv.R
@@ -61,6 +61,71 @@ output <- cbind(output, allData$pcaslMeanGMValue,allData$dti_jlf_tr_MeanTR)
 colnames(output) <- c('mprage_jlf_ct_MeanCT', 'pcasl_jlf_cbf_MeanWholeBrainCBF', 'rest_jlf_reho_MeanReho', 'rest_jlf_alff_MeanALFF', 'dti_jlf_tr_MeanWholeBrainTR', 'pcasl_jlf_cbf_MeanGMCBF', 'dti_jlf_tr_MeanGMTR')
 allData <- cbind(allData, output)
 
+# Now produce lobar values
+# Start with just volume
+lobarValues <- c(100, 101, 102, 103, 104, 105, 106, 107, 108,109, 104, 105, 108, 109, 100, 101, 104, 105, 104, 105, 122,123, 104, 105, 108, 109, 122, 123, 108, 109, 104, 105, 100, 101, 104, 105, 104, 105, 108, 109, 104, 105, 106, 107, 104, 105, 104, 105, 122, 123, 108, 109, 108, 109, 104, 105, 104, 105, 100, 101, 106, 107, 100, 101, 102, 103, 106, 107, 106, 107, 104, 105, 122, 123, 104, 105, 104, 105, 104, 105, 106, 107, 108, 109, 106, 107, 122, 123, 122, 123, 104, 105, 122, 123)
+volVals <- 104:201
+tmpVolVals <- allData[, volVals]
+tmpOut <- NULL
+for (v in names(table(lobarValues))){
+sumVal <- apply(tmpVolVals[, which(lobarValues == v)],1,function(x) sum(x, na.rm = T))
+tmpOut <- cbind(tmpOut, sumVal)
+}
+vol.lobes <- tmpOut
+
+# Now do it for all other modalities
+initVals <- c(223, 341, 440, 572, 702, 1221, 1338)
+initMods <- c("ct", "gmd", "cortcon", "cbf", "tr", "alff", "reho")
+allVals <- NULL
+for(p in 1:length(initVals)){
+  tmp.seq <- seq(initVals[p], initVals[p]+97)
+  tmp.dat <- allData[,tmp.seq]
+  tmpOut <- NULL
+  # Now produce the lobar values just like we did for the volume values, only these have to be volume weighted
+  for(v in names(table(lobarValues))){
+    tmp.dat.2 <- tmp.dat[,which(lobarValues == v)]
+    tmp.dat.vol <- tmpVolVals[, which(lobarValues == v)]
+    tmpOutVals <- NULL
+    for(z in 1:1601){
+      sqw <- weighted.mean(tmp.dat.2[z,], tmp.dat.vol[z,], na.rm=T)
+      tmpOutVals <- append(tmpOutVals, sqw)
+    }
+    tmpOut <- cbind(tmpOut, tmpOutVals)
+  }
+  allVals <- cbind(allVals, tmpOut)
+}
+# Now prepare the entire fil
+allOut <- cbind(vol.lobes, allVals)
+
+# Now prepare the column names
+templatVals <- c("%MODALITY%_jlfLobe_%MEASURE%_R_Limbic_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_L_Limbic_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_R_Insular_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_L_Insular_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_R_Frontal_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_L_Frontal_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_R_Parietal_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_L_Parietal_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_R_Occipital_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_L_Occipital_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_R_Temporal_Lobe",
+"%MODALITY%_jlfLobe_%MEASURE%_L_Temporal_Lobe")
+modVal <- c("mprage", "mprage", "mprage", "mprage", "pcasl", "dti", "rest", "rest")
+measureVal <- c("vol", "ct", "gmd", "cortcon", "cbf", "tr", "alff", "reho")
+colnames(allOut)[1:12] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[1]), pattern="%MEASURE%", replacement=measureVal[1])
+colnames(allOut)[13:24] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[2]), pattern="%MEASURE%", replacement=measureVal[2])
+colnames(allOut)[25:36] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[3]), pattern="%MEASURE%", replacement=measureVal[3])
+colnames(allOut)[37:48] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[4]), pattern="%MEASURE%", replacement=measureVal[4])
+colnames(allOut)[49:60] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[5]), pattern="%MEASURE%", replacement=measureVal[5])
+colnames(allOut)[61:72] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[6]), pattern="%MEASURE%", replacement=measureVal[6])
+colnames(allOut)[73:84] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[7]), pattern="%MEASURE%", replacement=measureVal[7])
+colnames(allOut)[85:96] <- gsub(gsub(templatVals, pattern="%MODALITY%", replacement=modVal[8]), pattern="%MEASURE%", replacement=measureVal[8])
+
+# Now attach it to all of the imaging data
+allData <- cbind(allData, allOut)
+allOut <- cbind(allData$bblid, allData$scanid, allOut)
+write.csv(allOut, "~/tmpVals.csv", quote=F, row.names=F)
+
 # Now attach the cognitive data
 modal.scores <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/cogData2017/20170308/CNB_Factor_Scores_GO1-GO2-GO3.csv',header=TRUE)
 modal.scores <- modal.scores[which(modal.scores$timepoint==1),]
@@ -72,7 +137,7 @@ demo.data <- merge(demo.data, race.vals, by=intersect(names(demo.data), names(ra
 demo.data <- merge(demo.data, ses.vals, by=intersect(names(demo.data), names(ses.vals)))
 all.data <- merge(demo.data, allData, by=intersect(names(demo.data), names(allData)), all=T)
 all.data <- all.data[all.data$bblid %in% allData$bblid,]
-fileName <- paste('/home/adrose/forRuben/data/n1601_imagingDataDump_', Sys.Date(), sep='')
+fileName <- paste('/home/adrose/forRuben/data/n1601_imagingDataDump_', Sys.Date(), '.csv', sep='')
 write.csv(all.data, fileName, quote=F, row.names=F)
 
 # Now limit it to only the subjects who we use in the hi Lo analysis
@@ -91,5 +156,5 @@ scanid.index <- data.values$scanid[acceptable.subjs]
 bblid.index <- bblid.index[bblid.index %in% health.values$bblid[which(health.values$incidentalExclude==0)]]
 bblid.index <- bblid.index[bblid.index %in% volume.data$bblid[which(volume.data$t1Exclude==0)]]
 all.data.hilo <- all.data[all.data$bblid %in% bblid.index,]
-fileName <- paste('/home/adrose/forRuben/data/n1601_hiLoDataDump_', Sys.Date(), sep='')
+fileName <- paste('/home/adrose/forRuben/data/n1601_hiLoDataDump_', Sys.Date(), '.csv', sep='')
 write.csv(all.data.hilo, fileName, quote=F, row.names=F)
