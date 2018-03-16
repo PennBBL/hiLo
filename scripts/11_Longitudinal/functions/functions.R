7cba28a54d419f070d5d63eee5d2949be4a81c92
Adon Rosen <adrose@galton.uphs.upenn.edu>
2018-03-16 17:16:04 -0400

    Added z score age regression

---
+++ scripts/11_Longitudinal/functions/functions.R
@@ -0,0 +1,101 @@
+## Create a function which will return the mean and standard deviation from 
+## age groups, whenn there are at least 20 to a group
+## To do this it will rrequire an input data frame
+## with a column with age in years
+## The output from this function will be a vector with the age bin index returned, the lnegth of the input vector
+returnAgeGroup <- function(ageValues, upperLimit=264){
+  # First thing is to collapse any age values greater then 22 into the 22 age bin
+  ageValues[ageValues>upperLimit] <- 264
+
+  # First grab the n values
+  nValues <- table(round(ageValues/12, digits=0))
+
+  # Now find any that have less than 20
+  smallCheck <- which(nValues<20)
+  scL <- length(smallCheck)
+  # If we have any with n less then 20 move that bin into it's closest neighbor  
+  while(scL>0){
+    # First take the smallest bin and inflate it + 1
+    binValue <- as.numeric(names(nValues)[smallCheck[1]])
+    # Now find the subjects with the lowest bin value and add 1 to them
+    valsToAdd <- which(round(ageValues/12, digits=0)==binValue)
+    vtaLC <- length(valsToAdd)
+    # Now add one to each of these 
+    while(vtaLC>0){
+      ageValues[valsToAdd] <- ageValues[valsToAdd] + 1
+      valsToAdd <- which(round(ageValues/12, digits=0)==binValue)
+      vtaLC <- length(valsToAdd)
+    }
+  # Now we need to again check to see if we satisfy errything
+  nValues <- table(round(ageValues/12, digits=0))
+  smallCheck <- which(nValues<20)
+  scL <- length(smallCheck)
+  }
+  # Now we need to return a vector with the bins that each person belongs to
+  # In order to do this we need loop thorugh every age value
+  # given in the nValues and return the age values that collapse within the 
+  # range for the value
+  loopValues <- as.numeric(names(nValues))
+  outputVector <- rep(NA, length(ageValues))
+  for(loopValue in loopValues){
+    outputVector[which(round(ageValues/12, digits=)==loopValue)] <- loopValue
+  } 
+  # Now return this guy
+  return(outputVector)
+}
+
+## Now I need to write a function which will output the mean and standard deviation 
+## for each of my age bins
+## The inputs to this function will be 
+returnMeanSDValues <- function(valueCol, ageCol, na.rm=TRUE){
+  # First initialize the output
+  outputMatrix <- matrix(NA, length(unique(ageCol)), 3)
+  # Now loop through each of the value Col and find the associated ageCol vals
+  # then simply return the mean and sd for each of em
+  rowValue <- 1
+  for(ageValue in sort(unique(ageCol))){
+    # First find the index for values 
+    indexVals <- which(ageCol==ageValue)
+    # Now find mean and sd
+    meanValue <- mean(valueCol[indexVals], na.rm=na.rm)
+    sdValue <- sd(valueCol[indexVals],na.rm=na.rm)
+    # Now export em
+    outputMatrix[rowValue,] <- c(ageValue,meanValue,sdValue)
+    rowValue <- rowValue + 1
+  }
+  # Now return the output matrix
+  return(outputMatrix)
+}
+
+## Now create a function which will z score all of the values within an age bin
+## Across all timepoints
+## The input to this function will be a 
+##  1. A matrix with the age values, mean, and standar deviation values
+##  2. The values to be z-scored
+##  3. the age groups
+applyMeanandSD <- function(ageMeanandSDMatrix, valueCol, ageCol){
+  ## The first thing we need to do is prepare the output
+  outputValues <- rep(NA, length(valueCol))
+  globalMean <- mean(valueCol, na.rm=TRUE)
+  ## Now we need to make sure that our age and value columns match up
+  if(length(valueCol)!=length(ageCol)){
+    stop("You done messed up a-aron")    
+  }
+  ## Now go through every value in the age index and z score them based on the 
+  ## appropriate age index
+  for(rowValue in seq(1, dim(ageMeanandSDMatrix)[1])){
+    ## First thing we need to do is find the indices for our age ppl
+    index <- which(ageCol==ageMeanandSDMatrix[rowValue,1])
+
+    ## Now z score the values based
+    scaledValues <- as.numeric(scale(valueCol[index], center=ageMeanandSDMatrix[rowValue,2], scale=ageMeanandSDMatrix[rowValue,3]))
+    
+    ## Now add the global mean back
+    scaledValues <- scaledValues + globalMean   
+ 
+    ## Now export these values to the output values vector
+    outputValues[index] <- scaledValues
+  }
+  ## Now return the outputValues
+  return(outputValues)
+}
--- scripts/11_Longitudinal/scripts/graphLongTrends.R
+++ scripts/11_Longitudinal/scripts/graphLongTrends.R
@@ -1,5 +1,10 @@
 ## Library(s)
-install_load('ggplot2')
+install_load('ggplot2', 'utils')
+source("~/hiLo/scripts/11_Longitudinal/functions/functions.R")
+
+## Now extend digit value in options
+options(digits=22)
+
 ## Data
 vol.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLR2416/volumeData.csv')
 vol.data$DOSCAN <- as.character(vol.data$DOSCAN)
@@ -9,6 +14,10 @@ gmd.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLR2416/gmdData.
 ct.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLR2416/ctData.csv')
 reho.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLR2416/rehoData.csv')
 alff.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLR2416/alffData.csv')
+all.data <- read.csv('/home/adrose/forRuben/data/n2416_imagingDataDump_2018-03-14.csv')
+all.data$DOSCAN <- as.character(all.data$DOSCAN)
+all.data$DOSCAN <- as.Date(all.data$DOSCAN, "%m/%d/%y")
+n1601.vals <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/n1601_demographics_go1_20161212.csv')
 ## The first thing I need to do is find the time from T1 in days
 vol.data$timeDiff <- 0
 # Now loop through each BBLID and find the time since time 1
@@ -28,30 +37,147 @@ for(i in unique(vol.data$bblid)){
 timeDiffVals <- output.vol[,c('bblid', 'scanid', 'timeDiff')]
 cbf.data <- merge(cbf.data, timeDiffVals)
 gmd.data <- merge(gmd.data, timeDiffVals)
+all.data <- merge(all.data, timeDiffVals)
+all.data$age <- scale(all.data$scanageMonths)
+all.data$age2 <- scale(all.data$age)^2
+all.data$age3 <- scale(all.data$age)^3
 
-# Now plot differences in TBV et cetra
-pasta.plot.one <- ggplot(output.vol[which(output.vol$sex==1),], aes(x=timeDiff, y=mprage_jlf_vol_ICV, group=bblid)) +
-  geom_point() +
-  theme_bw() +
-  theme(legend.position = "none") +
-  #geom_line() +
-  geom_smooth(method='gam',formula=y~x, level=0)
-pasta.plot.two <- ggplot(cbf.data[which(cbf.data$sex==1),], aes(x=timeDiff, y=pcaslMeanGMValue, group=bblid)) +
-  geom_point() +
-  theme_bw() +
-  theme(legend.position = "none") +
-  #geom_line() +
-  geom_smooth(method='gam',formula=y~x, level=0)
-pasta.plot.three <- ggplot(gmd.data[which(gmd.data$sex==1),], aes(x=timeDiff, y=mprage_jlf_gmd_MeanGMD, group=bblid)) +
-  geom_point() +
-  theme_bw() +
-  theme(legend.position = "none") +
-  #geom_line() +
-  geom_smooth(method='gam',formula=y~x, level=0)
+# Now produce the mean and sd values for errybody
+ageBinVals <- as.data.frame(cbind(n1601.vals$bblid, returnAgeGroup(n1601.vals$ageAtScan1)))
+colnames(ageBinVals) <- c('bblid', 'ageBin')
+all.data <- merge(all.data, ageBinVals, all=T)
 
+## Now produce the age regressed by z scoring values
+## This should be done within gender
+summaryMetrics <- c('mprage_jlf_vol_ICV', 'mprage_jlf_ct_MeanCT', 'mprage_jlf_gmd_MeanGMD', 'pcasl_jlf_cbf_MeanGMCBF', 'rest_jlf_reho_MeanReho', 'rest_jlf_alff_MeanALFF')
+all.data.male <- all.data[which(all.data$sex==1),]
+percent.diff.male.out <- all.data.male
+for(s in summaryMetrics){
+  percent.diff.male <- NULL
+  columnValue <- grep(s, names(all.data))
+  tmpMat <- returnMeanSDValues(all.data.male[,columnValue], all.data.male$ageBin)
+  all.data.male[,columnValue] <- applyMeanandSD(tmpMat, all.data.male[,columnValue], all.data.male$ageBin)
+  pb <- txtProgressBar(min=0, max=length(unique(all.data.male$bblid)), initial=0, style=3)
+  pbL <- 0
+  for(i in unique(all.data.male$bblid)){
+    ## Now find % difference 
+    tmpDat <- all.data.male[all.data.male$bblid==i,]
+    if(dim(tmpDat)[1]>1){
+      # Now if we have multiple time points find the minimum time, and compare everything to that guy
+      tmpDatMin <- tmpDat[which(tmpDat$DOSCAN==min(tmpDat$DOSCAN)),]
+      tmpDat[,columnValue] <- (tmpDat[,columnValue] - tmpDatMin[,columnValue])/tmpDatMin[,columnValue]
+    }
+    else{
+      tmpDat[,columnValue] <- 0
+    }
+    # Now combine everything
+    setTxtProgressBar(pb, pbL)
+    pbL <- pbL + 1
+    percent.diff.male <- rbind(percent.diff.male, tmpDat)
+  }
+  percent.diff.male.out[,columnValue] <- percent.diff.male[,columnValue]
+}
+## Now quickly apply some QA
+percent.diff.male.out$mprage_jlf_gmd_MeanGMD[which(percent.diff.male.out$mprage_jlf_gmd_MeanGMD< -10)] <- NA
+percent.diff.male.out$mprage_jlf_gmd_MeanGMD[which(percent.diff.male.out$mprage_jlf_gmd_MeanGMD> 10)] <- NA
 
-pdf('test.pdf', height=30, width=50)
-pasta.plot.one
-pasta.plot.two
-pasta.plot.three
+## Now do the same for females
+all.data.female <- all.data[which(all.data$sex==2),]
+percent.diff.female.out <- all.data.female
+for(s in summaryMetrics){
+  percent.diff.female <- NULL
+  columnValue <- grep(s, names(all.data))
+  tmpMat <- returnMeanSDValues(all.data.female[,columnValue], all.data.female$ageBin)
+  all.data.female[,columnValue] <- applyMeanandSD(tmpMat, all.data.female[,columnValue], all.data.female$ageBin)
+  pb <- txtProgressBar(min=0, max=length(unique(all.data.female$bblid)), initial=0, style=3)
+  pbL <- 0
+  for(i in unique(all.data.female$bblid)){
+    ## Now find % difference 
+    tmpDat <- all.data.female[all.data.female$bblid==i,]
+    if(dim(tmpDat)[1]>1){
+      # Now if we have multiple time points find the minimum time, and compare everything to that guy
+      tmpDatMin <- tmpDat[which(tmpDat$DOSCAN==min(tmpDat$DOSCAN)),]
+      tmpDat[,columnValue] <- (tmpDat[,columnValue] - tmpDatMin[,columnValue])/tmpDatMin[,columnValue]
+    }
+    else{
+      tmpDat[,columnValue] <- 0
+    }
+    # Now combine everything
+    setTxtProgressBar(pb, pbL)
+    pbL <- pbL + 1
+    percent.diff.female <- rbind(percent.diff.female, tmpDat)
+  }
+  percent.diff.female.out[,columnValue] <- percent.diff.female[,columnValue]
+}
+## Now do some QA
+percent.diff.female.out$mprage_jlf_gmd_MeanGMD[which(percent.diff.female.out$mprage_jlf_gmd_MeanGMD< -10)] <- NA
+percent.diff.female.out$mprage_jlf_gmd_MeanGMD[which(percent.diff.female.out$mprage_jlf_gmd_MeanGMD> 10)] <- NA
+percent.diff.female.out$rest_jlf_reho_MeanReho[which(percent.diff.female.out$rest_jlf_reho_MeanReho< -10)] <- NA
+percent.diff.female.out$rest_jlf_reho_MeanReho[which(percent.diff.female.out$rest_jlf_reho_MeanReho> 10)] <- NA
+
+# Now isolate the data to those subjects we are only interested in
+# First do the TD and emergent comparison
+graph.vals.one <- percent.diff.male.out[which(percent.diff.male.out$pncGrpPsychosisCl=="TD" | percent.diff.male.out$pncGrpPsychosisCl=="Emergent"),]
+graph.vals.two <- percent.diff.male.out[which(percent.diff.male.out$pncGrpPsychosisCl=="Persister" | percent.diff.male.out$pncGrpPsychosisCl=="Resilient"),]
+
+options(digits=7)
+pdf('maleRawValuesTvEAR.pdf')
+for(i in summaryMetrics){
+  colVal <- grep(i, names(graph.vals.one))
+  toPlot <- graph.vals.one[complete.cases(graph.vals.one[,colVal]),]
+  pasta.plot.one <- ggplot(toPlot[which(toPlot$sex==1),], aes(x=timeDiff, y=toPlot[which(toPlot$sex==1),colVal])) + 
+    geom_point() + 
+    geom_line(aes(group=bblid, col=pncGrpPsychosisCl, alpha=.1)) +
+    geom_smooth(method='gam',aes(group=pncGrpPsychosisCl, col=pncGrpPsychosisCl)) +
+    ylab(i) +
+    theme_bw()
+  print(pasta.plot.one)
+}
+dev.off()
+
+## pers vs res
+pdf('maleRawValuesPvRAR.pdf')
+for(i in summaryMetrics){
+  colVal <- grep(i, names(graph.vals.one))
+  toPlot <- graph.vals.two[complete.cases(graph.vals.two[,colVal]),]
+  pasta.plot.one <- ggplot(toPlot[which(toPlot$sex==1),], aes(x=timeDiff, y=toPlot[which(toPlot$sex==1),colVal])) + 
+    geom_point() + 
+    geom_line(aes(group=bblid, col=pncGrpPsychosisCl, alpha=.1)) +
+    geom_smooth(method='gam',aes(group=pncGrpPsychosisCl, col=pncGrpPsychosisCl)) +
+    ylab(i) +
+    theme_bw()
+  print(pasta.plot.one)
+}
+dev.off()
+
+## Now do females
+graph.vals.one <- percent.diff.female.out[which(percent.diff.female.out$pncGrpPsychosisCl=="TD" | percent.diff.female.out$pncGrpPsychosisCl=="Emergent"),]
+graph.vals.two <- percent.diff.female.out[which(percent.diff.female.out$pncGrpPsychosisCl=="Persister" | percent.diff.female.out$pncGrpPsychosisCl=="Resilient"),]
+
+pdf('femaleRawValuesTvEAR.pdf')
+for(i in summaryMetrics){
+  colVal <- grep(i, names(graph.vals.one))
+  toPlot <- graph.vals.one[complete.cases(graph.vals.one[,colVal]),]
+  pasta.plot.one <- ggplot(toPlot[which(toPlot$sex==2),], aes(x=timeDiff, y=toPlot[which(toPlot$sex==2),colVal])) + 
+    geom_point() + 
+    geom_line(aes(group=bblid, col=pncGrpPsychosisCl, alpha=.1)) +
+    geom_smooth(method='gam',aes(group=pncGrpPsychosisCl, col=pncGrpPsychosisCl)) +
+    ylab(i) +
+    theme_bw()
+  print(pasta.plot.one)
+}
+dev.off()
+
+pdf('femaleRawValuesPvRAR.pdf')
+for(i in summaryMetrics){
+  colVal <- grep(i, names(graph.vals.one))
+  toPlot <- graph.vals.two[complete.cases(graph.vals.two[,colVal]),]
+  pasta.plot.one <- ggplot(toPlot[which(toPlot$sex==2),], aes(x=timeDiff, y=toPlot[which(toPlot$sex==2),colVal])) + 
+    geom_point() + 
+    geom_line(aes(group=bblid, col=pncGrpPsychosisCl, alpha=.1)) +
+    geom_smooth(method='gam',aes(group=pncGrpPsychosisCl, col=pncGrpPsychosisCl)) +
+    ylab(i) +
+    theme_bw()
+  print(pasta.plot.one)
+}
 dev.off()
