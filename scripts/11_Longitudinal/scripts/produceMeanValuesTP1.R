9f580642de338a571a2a560ff26df54c2f3495ca
Adon Rosen <adrose@galton.uphs.upenn.edu>
2018-05-26 12:37:41 -0400

    Added ERS and PS quartile groups

--- scripts/11_Longitudinal/scripts/produceMeanValuesTP1.R
+++ scripts/11_Longitudinal/scripts/produceMeanValuesTP1.R
@@ -239,6 +239,33 @@ createGGPlotImage <- function(dataFrame, plotTitle, minValue, maxValue){
   return(plotToReturn)
 }
 
+## Now create a function which will plot all of our values
+createGGPlotImage2 <- function(dataFrame, plotTitle, minValue, maxValue){
+  plotToReturn <- ggplot(dataFrame, aes(y=value, x=ROI, group=group)) +
+      geom_line(aes(linetype=group,color=group), size=1.5) +
+      geom_point(aes(shape=group, color=group), size=1.5) +
+      geom_errorbar(aes(x=ROI, ymin=value-se, ymax=value+se, color=group)) + 
+      scale_y_continuous(limits=c(minValue, maxValue), 
+                           breaks=round(seq(minValue, maxValue,.25), digits=2)) +
+      xlab("ROI") +
+      ylab("Effect Size") +
+      geom_hline(aes(yintercept=0), linetype="solid", colour="black", size=0.5) +
+      scale_colour_manual(name = "group",
+                          values=c("High ERS // High Psychosis"="orange","High ERS // Low Psychosis"="light blue","Low ERS // High Psychosis"="red","Low ERS // Low Psychosis"="blue")) +
+      scale_linetype_manual(name = "group", values = c("High ERS // High Psychosis"="dotted","High ERS // Low Psychosis"="dotted","Low ERS // High Psychosis"="solid","Low ERS // Low Psychosis"="solid")) +
+      scale_shape_manual(name="group", values=c("High ERS // High Psychosis"="18","High ERS // Low Psychosis"="18","Low ERS // High Psychosis"="17","Low ERS // Low Psychosis"="9")) +
+      theme_bw() +
+      theme(legend.position="top") +
+      theme(text=element_text(size=20), axis.text.x = element_text(angle = 45, hjust = 1, face="bold"), 
+      axis.text.y = element_text(face="bold", size=24), axis.title.x = element_text(face="bold", size=28),
+      axis.title.y = element_text(face="bold", size=28),
+      plot.title = element_text(face="bold", size=28), strip.text.x = element_text(size=15)) +
+      facet_grid(. ~ lobe, scales="free", space="free_x") +
+      ggtitle(plotTitle)
+  
+  return(plotToReturn)
+}
+
 ## Load the data
 img.data <-  read.csv("/home/adrose/forRuben/data/n2416_imagingDataDump_2018-05-04.csv")
 long.labels <- read.csv("/home/adrose/dataPrepForHiLoPaper/data/n2416ClinicalDemoPsycho/pnc_diagnosis_categorical_20170526.csv")
@@ -413,3 +440,100 @@ for(m in modalities){
   print(m)
   index <- index + 1
 }
+
+## Now do our PS and ERS quartile bins
+## First load our ERS and PS data
+bin.cont.data <- read.csv('/home/tymoore/Gene-Environment_Long_Data_for_Ruben_3.csv')
+# Isolate our cognitive domain to only one value
+bin.cont.data <- bin.cont.data[which(bin.cont.data$Cognitive_Domain=="abf_efficiency"),]
+img.data.2 <- merge(img.data, bin.cont.data, by='bblid')
+img.data.2 <- img.data.2[which(img.data.2$tpvalue==1),]
+## Now prepare our imaging values
+names.index <- names(all.data)[grep('jlf', names(all.data))]
+
+## Age regress the values here
+img.values.ar <- img.data.2[,names.index]
+img.values.ar$group <- img.data.2$ERS_Psychosis_Quartile
+img.values.ar$sex <- img.data.2$sex
+img.values.ar$age <- img.data.2$scanageMonths
+img.values.ar[,names.index] <- apply(img.values.ar[,names.index], 2, function(x) regressOutAge(x, img.values.ar$age)) 
+
+## Now average across hemispheres
+img.values.ar <- averageLeftAndRight(img.values.ar)
+
+## Now scale these values
+names.index <- names(img.values.ar)[grep('jlf', names(img.values.ar))]
+img.values.ar.male <- apply(img.values.ar[which(img.values.ar$sex==1),names.index], 2, function(x) as.numeric(scale(x)))
+img.values.ar.female <- apply(img.values.ar[which(img.values.ar$sex==2),names.index], 2, function(x) as.numeric(scale(x)))
+img.values.ar[which(img.values.ar$sex==1),names.index] <- img.values.ar.male
+img.values.ar[which(img.values.ar$sex==2),names.index] <- img.values.ar.female
+img.values.ar <- data.frame(img.values.ar)
+img.values.ar <- img.values.ar[-which(img.values.ar$group==''),]
+
+## Now calculate the mean values for each of our labels for these guys
+output.data.frame <- NULL
+for(i in names.index){
+  tmp.data <- melt(summarySE(data=img.values.ar, measurevar=i, groupvars=c('group'), na.rm=T), id=c('group', 'N', 'se', 'sd', 'ci'), measure.vars=c(i))
+  output.data.frame <- rbind(output.data.frame, tmp.data)
+}
+output.data.frame$modality <- NA
+## Now find the attach a modality factor
+modalities <- c('vol', 'ct', 'gmd', 'cortcon', 'cbf', 'tr', 'rd', 'ad', 'fa', 'alff', 'reho')
+for(m in modalities){
+  # create our grep pattern
+  gp <- paste("_", m, "_", sep='')
+  output.data.frame$modality[grep(gp, output.data.frame$variable)] <- m
+}
+
+## Now attach our pretty names
+output.data.frame$lobe <- NA
+for(i in unique(output.data.frame$variable)){
+  output.data.frame$lobe[which(output.data.frame$variable==i)] <- findLobe(i) 
+}
+output.data.frame$lobe[output.data.frame$lobe==1] <- "Basal Ganglia"
+output.data.frame$lobe[output.data.frame$lobe==2] <- "Limbic"
+output.data.frame$lobe[output.data.frame$lobe==3] <- "Frontal Orbital"
+output.data.frame$lobe[output.data.frame$lobe==4] <- "Frontal Dorsal"
+output.data.frame$lobe[output.data.frame$lobe==5] <- "Temporal"
+output.data.frame$lobe[output.data.frame$lobe==6] <- "Parietal"
+output.data.frame$lobe[output.data.frame$lobe==7] <- "Occipital"
+output.data.frame$lobe[output.data.frame$lobe==8] <- "Cerebellum"
+output.data.frame$lobe[output.data.frame$lobe==9] <- "WM Lobe"
+
+## Now isolate our individual modalities and produce our pretty names
+lobes.to.iter <- c("Basal Ganglia","Limbic","Frontal Orbital","Frontal Dorsal","Temporal","Parietal","Occipital","Cerebellum")
+modalities <- c('vol', 'gmd','cbf', 'tr','alff', 'reho')
+min.val <- c(-.7,-.5,-.5,-.6,-.5,-.5)
+max.val <- c(.8,.5,.6,.9,.8,.7)
+index <- 1
+for(m in modalities){
+  tmp.data <- output.data.frame[which(output.data.frame$modality==m),]
+  tmp.vals.to.plot <- NULL
+  if(m %in% c('vol', 'gmd', 'tr', 'reho', 'alff')){
+    for(x in lobes.to.iter){
+      # First produce isolate our modality of interest
+      new.values <- reorganizeLobeOrder(tmp.data, x)
+      tmp.vals.to.plot <- rbind(tmp.vals.to.plot, new.values)
+    }
+    tmp.vals.to.plot$ROI <- revalue(tmp.vals.to.plot$ROI, c("Putamen"="Put", "Accumbens_Area"="NA", "Pallidum"="GP","Thalamus_Proper"="Tha","Caudate"="CN"))
+    tmp.vals.to.plot$ROI <- revalue(tmp.vals.to.plot$ROI,c("Cerebellum_Exterior"="Cer Ext", "Cerebellar_Vermal_Lobules_I.V"="Vermis 1-5", "Cerebellar_Vermal_Lobules_VI.VII"="Vermis 6-7", "Cerebellar_Vermal_Lobules_VIII.X"="Vermis 8-10"))
+  }
+  else{
+    for(x in lobes.to.iter[1:7]){
+      # First produce isolate our modality of interest
+      new.values <- reorganizeLobeOrder(tmp.data, x)
+      tmp.vals.to.plot <- rbind(tmp.vals.to.plot, new.values)
+    }
+    tmp.vals.to.plot$ROI <- revalue(tmp.vals.to.plot$ROI, c("Putamen"="Put", "Accumbens_Area"="NA", "Pallidum"="GP","Thalamus_Proper"="Tha","Caudate"="CN"))
+  }
+  
+  tmp.vals.to.plot$lobe <- factor(tmp.vals.to.plot$lobe, levels=c("Basal Ganglia","Limbic", "Frontal Orbital", "Frontal Dorsal", "Temporal", "Parietal","Occipital", "Cerebellum"))
+  # Now create our plots
+  out.plot <- createGGPlotImage2(dataFrame=tmp.vals.to.plot, plotTitle='',minValue=min.val[index], maxValue=max.val[index])
+  pdf(paste(m, "ERSandPSQuartiles.pdf", sep=''), height=12, width=18)
+  print(out.plot)
+  dev.off()
+  print(m)
+  index <- index + 1
+}
+
