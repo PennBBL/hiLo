cbeca41640c47bff69613acb14c0351eb8dc042f
Adon Rosen <adrose@galton.uphs.upenn.edu>
2019-01-04 12:32:00 -0500

    Added female

--- scripts/08_importanceExplore/scripts/combineAllImportMetrics.R
+++ scripts/08_importanceExplore/scripts/combineAllImportMetrics.R
@@ -5,7 +5,7 @@ install_load('ggplot2', 'GGally')
 source('../functions/functions.R')
 ## The first thing we have to do is produce all of the importance metrics
 ## Little bash fandangling
-system("for i in `ls prep*R` ; do Rscript ${i} ; done")
+system("for i in `ls prep*R` ; do Rscript ${i} & done")
 
 ## Now we will read all of the male values and produce the correlation matrix
 randImp <- read.csv('./randForImpMale.csv')
@@ -47,6 +47,52 @@ all.data$modality[263:322] <- 'ALFF'
 all.data$modality[323:382] <- 'REHO'
 all.data$modality <- factor(all.data$modality)
 
-pdf('testOut.pdf', height=20, width=20)
-ggpairs(data=all.data,columns=c(2,4:7),ggplot2::aes(colour=modality))
+pdf('scaleRidgeMal.pdf', width=16, height=16)
+ggpairs(data=all.data,columns=c(2,6:7),ggplot2::aes(colour=modality))
+ggpairs(data=all.data[grep("mprage_jlf_vol", all.data$ROI_readable),],columns=c(2,6:7),ggplot2::aes(colour=modality))
+dev.off()
+
+## Now we will read all of the male values and produce the correlation matrix
+randImp <- read.csv('./randForImpFemale.csv')
+colnames(randImp) <- c("ROI_readable", "randomForestImp", "modality", "ModelingTechniqueRF")
+randImp <- randImp[-which(randImp$modality=='all.data'),]
+randImp <- randImp[,-c(3,4)]
+relifImp <- read.csv('./reliefFImpFemale.csv')
+colnames(relifImp) <- c("ROI_readable", "reliefImp", "modality", "ModelingTechniqueRF")
+relifImp <- relifImp[-which(relifImp$modality=='all.data'),]
+relifImp <- relifImp[,-c(3,4)]
+ridgeImp <- read.csv('./ridgeImpFemale.csv')
+colnames(ridgeImp) <- c("ROI_readable", "ridgeImp", "modality", "ModelingTechniqueRidge")
+ridgeImp <- ridgeImp[-which(ridgeImp$modality=='all.data'),]
+ridgeImp <- ridgeImp[,-c(3,4)]
+sregImp <- read.csv('selfRegImpFemale.csv')
+sregImp[,1] <- gsub(sregImp[,1], pattern='tmpDatX', replacement='')
+colnames(sregImp) <- c("ROI_readable", "srelfRegImp", "modality", "ModelingTechniqueSR")
+sregImp <- sregImp[-which(sregImp$modality=='all.data'),]
+sregImp <- sregImp[,-c(3,4)]
+efImp <- read.csv('./effSizeImp.csv')
+
+## Now isolate the female eff size variables
+efImp <- efImp[which(efImp$sex=='F'),]
+
+## Now combine all of these
+all.data <- merge(efImp, randImp)
+all.data <- merge(all.data, relifImp, by='ROI_readable')
+all.data <- merge(all.data, ridgeImp, by='ROI_readable')
+all.data <- merge(all.data, sregImp, by='ROI_readable')
+
+## Now add a modality vairbale to the data
+all.data$modality <- NA
+all.data$modality[1:26] <- 'FA'
+all.data$modality[27:86] <- 'TR'
+all.data$modality[87:146] <- 'GMD'
+all.data$modality[147:206] <- 'VOL'
+all.data$modality[207:262] <- 'CBF'
+all.data$modality[263:322] <- 'ALFF'
+all.data$modality[323:382] <- 'REHO'
+all.data$modality <- factor(all.data$modality)
+
+pdf('scaleRidgeFem.pdf', width=16, height=16)
+ggpairs(data=all.data,columns=c(2,6:7),ggplot2::aes(colour=modality))
+ggpairs(data=all.data[grep("mprage_jlf_vol", all.data$ROI_readable),],columns=c(2,6:7),ggplot2::aes(colour=modality))
 dev.off()
