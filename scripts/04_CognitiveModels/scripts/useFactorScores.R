8cf4bb130c19701a92e0d4fb555492d33d26d08a
Adon Rosen <adrose@galton.uphs.upenn.edu>
2019-01-04 12:28:29 -0500

    Reading in diff data

--- scripts/04_CognitiveModels/scripts/useFactorScores.R
+++ scripts/04_CognitiveModels/scripts/useFactorScores.R
@@ -4,17 +4,21 @@ install_load('foreach', 'doParallel', 'glmnet','psych','reshape2', 'caret','MASS
 
 # Load the data 
 factorScores <- read.csv('/home/adrose/Brain_Factor_Scores_Volume_GMD_Cortical-Thickness.csv')
+factorScoresFunc <- read.csv('/home/adrose/brain_factor_scores_reho_alff.csv')
+#factorScores <- merge(factorScores, factorScoresFunc)
 vol.data <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/meanLRVolandAgeReg/volumeData.csv')
 allDat <- merge(factorScores, vol.data)
+allDatF <- merge(factorScoresFunc, vol.data)
 
 # Now I need to produce the CV r-squared using ridge regression for every modalities factor scores
 
-dataGrepNames <- c('VOL_', 'GMD', 'CorThickness_')
+dataGrepNames <- c('Volume_', 'Cortical_Thickness_', 'ReHo_', 'ALFF_')
+dataNames <- c('allDat', 'allDat', 'allDatF', 'allDatF')
 allRVals <- NULL
 for(emptyVal in 1:10){
 for(g in 1:2){
   for(z in 1:length(dataGrepNames)){
-    tmpDF <- allDat
+    tmpDF <- get(dataNames[z])
     tmpDF <- tmpDF[which(tmpDF$sex==g),]
     tmp.col <- grep(dataGrepNames[z], names(tmpDF))
     tmp.values <- scale(tmpDF[,tmp.col])[,1:length(tmp.col)]
