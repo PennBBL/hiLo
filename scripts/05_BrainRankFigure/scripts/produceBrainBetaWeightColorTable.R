2880fb356a436cbaa7c96439d842f86d999803af
Adon Rosen <adrose@galton.uphs.upenn.edu>
2019-04-09 10:26:00 -0400

    Raw beta coef

--- scripts/05_BrainRankFigure/scripts/produceBrainBetaWeightColorTable.R
+++ scripts/05_BrainRankFigure/scripts/produceBrainBetaWeightColorTable.R
@@ -2,16 +2,70 @@
 source('/home/adrose/hiLo/scripts/05_BrainRankFigure/functions/functions.R')
 
 ## Load data
-all.dat <- read.csv('../../04_CognitiveModels/scripts/F1_Exec_Comp_Cog_AccuracyBetaWeights.csv')
+all.dat <- read.csv('../../08_importanceExplore/scripts/ridgeImpMale.csv')
+all.dat$X <- as.character(all.dat$X)
+## Now I need to fix the meaned hemisphere thing I got going on for the output scripts
+left.vals <- all.dat
+left.vals$X <- gsub(x=left.vals$X, pattern='mprage_jlf_vol_', replacement='mprage_jlf_vol_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='mprage_jlf_gmd_', replacement='mprage_jlf_gmd_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='pcasl_jlf_cbf_', replacement='pcasl_jlf_cbf_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='dti_jlf_tr_', replacement='dti_jlf_tr_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='rest_jlf_alff_', replacement='rest_jlf_alff_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='rest_jlf_reho_', replacement='rest_jlf_reho_L_')
+
+## Now do the same thing for the right
+right.vals <- all.dat
+right.vals$X <- gsub(x=right.vals$X, pattern='mprage_jlf_vol_', replacement='mprage_jlf_vol_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='mprage_jlf_gmd_', replacement='mprage_jlf_gmd_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='pcasl_jlf_cbf_', replacement='pcasl_jlf_cbf_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='dti_jlf_tr_', replacement='dti_jlf_tr_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='rest_jlf_alff_', replacement='rest_jlf_alff_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='rest_jlf_reho_', replacement='rest_jlf_reho_R_')
+
+## Now combine all of these
+all.dat <- rbind(all.dat, left.vals, right.vals)
 
 ## Now produce each modalities beta weight color table
-maxVal <- 4.8
-minVal <- -2
-for(i in 2:13){
+maxVal <- 3
+minVal <- -3
+grep.vals <- c('vol', 'cbf', 'gmd', 'tr', 'fa', 'all.data', 'reho', 'alff')
+for(i in grep.vals){
   # First isolate our values
-  toMake <- all.dat[,c(1, i)]
-  # Now grab the col name
-  outputFile <- colnames(all.dat)[i]
+  toMake <- all.dat[grep(i , all.dat$X.1),]
   # Now produce the color table
-  writeColorTableandKey(inputData=toMake, inputColumn=2, outName=outputFile, minTmp=c(minVal, 0), maxTmp=c(0, maxVal))
+  writeColorTableandKey(inputData=toMake, inputColumn=2, outName=paste("male",i, sep=''), minTmp=c(minVal, 0), maxTmp=c(0, maxVal))
+}
+
+## Now do females
+all.dat <- read.csv('../../08_importanceExplore/scripts/ridgeImpFemale.csv')
+all.dat$X <- as.character(all.dat$X)
+## Now I need to fix the meaned hemisphere thing I got going on for the output scripts
+left.vals <- all.dat
+left.vals$X <- gsub(x=left.vals$X, pattern='mprage_jlf_vol_', replacement='mprage_jlf_vol_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='mprage_jlf_gmd_', replacement='mprage_jlf_gmd_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='pcasl_jlf_cbf_', replacement='pcasl_jlf_cbf_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='dti_jlf_tr_', replacement='dti_jlf_tr_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='rest_jlf_alff_', replacement='rest_jlf_alff_L_')
+left.vals$X <- gsub(x=left.vals$X, pattern='rest_jlf_reho_', replacement='rest_jlf_reho_L_')
+
+## Now do the same thing for the right
+right.vals <- all.dat
+right.vals$X <- gsub(x=right.vals$X, pattern='mprage_jlf_vol_', replacement='mprage_jlf_vol_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='mprage_jlf_gmd_', replacement='mprage_jlf_gmd_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='pcasl_jlf_cbf_', replacement='pcasl_jlf_cbf_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='dti_jlf_tr_', replacement='dti_jlf_tr_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='rest_jlf_alff_', replacement='rest_jlf_alff_R_')
+right.vals$X <- gsub(x=right.vals$X, pattern='rest_jlf_reho_', replacement='rest_jlf_reho_R_')
+
+## Now combine all of these
+all.dat <- rbind(all.dat, left.vals, right.vals)
+## Now produce each modalities beta weight color table
+maxVal <- 3
+minVal <- -3
+grep.vals <- c('vol', 'cbf', 'gmd', 'tr', 'fa', 'all.data', 'reho', 'alff')
+for(i in grep.vals){
+  # First isolate our values
+  toMake <- all.dat[grep(i , all.dat$X.1),]
+  # Now produce the color table
+  writeColorTableandKey(inputData=toMake, inputColumn=2, outName=paste("female",i, sep=''), minTmp=c(minVal, 0), maxTmp=c(0, maxVal))
 }
