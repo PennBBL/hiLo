890683e7119b966495f6a7eb0aece8cf9d8ec868
Ellyn Butler <ellynrbutler@gmail.com>
2020-06-01 12:15:24 -0400

    Subcortical results

--- scripts/13_FigureProduction/scripts/ggseg_figure_ExtPFIT_rank.R
+++ scripts/13_FigureProduction/scripts/ggseg_figure_ExtPFIT_rank.R
@@ -1,12 +1,12 @@
 ### This script creates (hopefully) the final ggseg figure
 ###
 ### Ellyn Butler
-### January 29, 2020 - May 29, 2020
+### January 29, 2020 - June 1, 2020
 
 library('ggseg')
 library('ggplot2')
 library('dplyr')
-library('ggpattern')
+library('ggpubr')
 library('R.utils')
 
 load("~/Documents/ggsegExtra/data/micCort.rda")
@@ -71,15 +71,70 @@ p <- ggseg(final_data, atlas="micCort", mapping=aes(fill=Top2), hemisphere="left
   colour="black") +
   scale_fill_manual(values=c("#2166ac", "#4393c3", "#92c5de", "#d1e5f0",
     "aliceblue", "#67001f", "#b2182b", "cornsilk"), drop=FALSE) +
+  #labs(fill="Ranked Top #") +
+  theme(text=element_text(size=14,  family="Arial"), axis.title.x=element_blank(), axis.text.x=element_blank(), legend.position="none")
+
+
+###############################################################################
+
+aseg_data <- aseg
+
+rank_df2 <- rank_df
+rank_df2$Abbrev <- recode(rank_df2$Abbrev, "Hipp"="hippocampus", "Thal"="thalamus proper",
+  "Put"="putamen", "Amy"="amygdala", "Pall"="pallidum", "Cau"="caudate")
+
+aseg_data$PFIT <- as.character(aseg_data$PFIT)
+aseg_data$Important <- as.character(aseg_data$Important)
+aseg_data$Top <- as.numeric(aseg_data$Top)
+
+PFITregions <- c("caudate")
+ExtPFITregions <- c("hippocampus", "thalamus proper")
+
+for (i in 1:nrow(aseg_data)) {
+  area <- as.character(aseg_data[i, "area"])
+  if (!is.na(area) & area %in% unique(rank_df2$Abbrev)) {
+    if (area %in% PFITregions) {
+      aseg_data[i, "PFIT"] <- "Yes"
+      aseg_data[i, "Important"] <- "Yes"
+    } else if (area %in% ExtPFITregions) {
+      aseg_data[i, "PFIT"] <- "No"
+      aseg_data[i, "Important"] <- "Yes"
+    } else {
+      aseg_data[i, "PFIT"] <- "No"
+      aseg_data[i, "Important"] <- "No"
+    }
+    aseg_data[i, "Top"] <- rank_df2[rank_df2$Abbrev == area, "RankGroup"]
+  }
+}
+
+aseg_data$Top <- as.character(aseg_data$Top)
+aseg_data$Top2 <- paste0(aseg_data$Important, aseg_data$PFIT, aseg_data$Top)
+aseg_data$Top2 <- recode(aseg_data$Top2, "YesYes10"="PFIT 10", "YesYes40"="PFIT 40",
+  "YesNo10"="ExtPFIT 10", "NoNo40"="Other", "NoNo50"="Other", "NoNo30"="Other",
+  "YesYes20"="PFIT 20", "YesNo20"="ExtPFIT 20", "NoNo60"="Other",
+  "YesYes50"="PFIT 50")
+aseg_data$Top2 <- ordered(aseg_data$Top2, c("PFIT 10", "PFIT 20", "PFIT 30",
+  "PFIT 40", "PFIT 50", "ExtPFIT 10", "ExtPFIT 20", "Other"))
+aseg_data <- aseg_data[!is.na(aseg_data$Top2),]
+
+
+p_subcort <- ggseg(aseg_data, atlas="aseg", hemisphere=c("left", "right"),
+  mapping=aes(fill=Top2), size=.1, colour="black", na.rm=TRUE) +
+  scale_fill_manual(values=c("#2166ac", "#4393c3", "#92c5de", "#d1e5f0",
+    "aliceblue", "#67001f", "#b2182b", "cornsilk"), labels=c("PFIT 10", "PFIT 20",
+    "PFIT 30", "PFIT 40", "PFIT 50", "ExtPFIT 10", "ExtPFIT 20", "Other"), drop=FALSE) +
   labs(fill="Ranked Top #") +
-  theme(text=element_text(size=14,  family="Arial"), axis.title.x=element_blank(), axis.text.x=element_blank())
+  theme(text=element_text(size=14,  family="Arial"), axis.title.x=element_blank(),
+    axis.text.x=element_blank())
 
-png(file="~/Documents/hiLo/plots/figure2_color_ExtPFIT_rank.png", units="mm", width=180, height=100, res=800)
-p
+
+
+
+
+
+
+
+
+png(file="~/Documents/hiLo/plots/figure2_color_ExtPFIT_rank.png", units="mm", width=150, height=140, res=800)
+ggarrange(p, p_subcort, nrow= 2, labels = c("A", "B"))
 dev.off()
-
-
-
-scale_fill_manual(values=c("antiquewhite1", "#fddbc7", "#f4a582", "#d6604d",
-  "#b2182b", "#67001f", "black", "white", "aliceblue", "#d1e5f0", "#92c5de",
-  "#4393c3", "#2166ac", "#053061", "black"), drop=FALSE) +
